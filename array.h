34b1d7cac56892490bb48f03c5b66cc00672571f
doug-h <hull.douglasj@gmail.com>
2024-07-03 18:26:55 +0100

    Array slice method

--- array.h
+++ array.h
@@ -22,6 +22,8 @@ struct array {
   void erase(iZ i) { base[i] = *--tail; }
   void erase(T* t) { ASSERT(t >= base && t < tail); *t = *--tail; }
 
+  array<T> slice(iZ, iZ) const;
+
   T* first() const { return base; }
   T* last() const { return (tail - 1); }
 
@@ -41,6 +43,19 @@ struct array {
   }
 };
 
+template <class T>
+array<T> array<T>::slice(iZ start, iZ end) const {
+  start = (start < 0) ? start + this->count() : start;
+  end = (end <= 0) ? end + this->count() : end;
+
+  array<T> result;
+  result.base = this->base + start;
+  result.tail = this->base + end;
+  ASSERT(result.base <= result.tail);
+  ASSERT(result.base >= this->base);
+  ASSERT(result.tail <= this->tail);
+  return result;
+}
 template <class T>
 T* array<T>::push(array<T> other) {
   ASSERT(other.count() > 0);
